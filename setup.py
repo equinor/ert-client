fddbbf3133ea9c9d9087aa05d010656fc3cae5bd
Zohar Malamant <zom@equinor.com>
2020-09-15 15:17:30 +0200

    Basic project skeleton

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from setuptools_scm import get_version
+
+
+setup(
+    name="ert-client",
+    author="Equinor ASA",
+    description="ERT API client library",
+    url="https://github.com/Equinor/ert-client",
+    packages=find_packages(exclude=["tests"]),
+    license="GPL-3.0",
+    platforms="any",
+    install_requires=[],
+    version=get_version(relative_to=__file__, write_to="ertapi/__version__.py"),
+)
